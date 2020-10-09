35485f3bf722cc68be585cc239045559199473ff
dnsmsv <dnsmsv@gmail.com>
2020-10-09 18:39:38 +0300

    Added build

---
+++ build/precache-manifest.68115c032ad59ca3939b41591fcad568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d29639df0ee2d40a069bf111ee47a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf26ac9427a78c49db1",
+    "url": "/static/css/main.e505df59.chunk.css"
+  },
+  {
+    "revision": "f2c55b120a4b158ccd83",
+    "url": "/static/js/2.7f8866c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f8866c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf26ac9427a78c49db1",
+    "url": "/static/js/main.ab77c498.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
