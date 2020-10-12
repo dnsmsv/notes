8d183291eb1c0157077503ec0d6e94023727ab84
Denis Moiseev <dnsmsv@gmail.com>
2020-10-12 15:47:07 +0300

    Edited readme file. Preparing routes for lounch

---
+++ build/precache-manifest.2e5a7fba4113ba588b4cf97cdb8fdc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d916c3ecc3b43e51f1a08d05465bb00",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "bc3a952f08822334dde9",
+    "url": "/notes/static/css/main.e505df59.chunk.css"
+  },
+  {
+    "revision": "f2c55b120a4b158ccd83",
+    "url": "/notes/static/js/2.7f8866c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notes/static/js/2.7f8866c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3a952f08822334dde9",
+    "url": "/notes/static/js/main.013d5ed2.chunk.js"
+  },
+  {
+    "revision": "b4e7f333cec4847613e3",
+    "url": "/notes/static/js/runtime-main.9f0c36eb.js"
+  }
+]);
